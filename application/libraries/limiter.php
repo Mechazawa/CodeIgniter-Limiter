1eec7c8fa199d4883d69a21322f3eb129d60f88c
Bas <mega@ioexception.at>
2015-01-13 16:40:16 +0100

    Remove table sql from Limiter

--- application/libraries/limiter.php
+++ application/libraries/limiter.php
@@ -10,16 +10,6 @@
  */
 
 
-/** -- Database --
- * CREATE TABLE `rate_limit` (
- * `client` varchar(255) NOT NULL DEFAULT '',
- * `target` varchar(255) NOT NULL DEFAULT '_global',
- * `start` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
- * `count` int(11) NOT NULL DEFAULT '1',
- * PRIMARY KEY (`client`,`target`)
- * ) ENGINE=InnoDB DEFAULT CHARSET=ascii;
- */
-
 class Limiter {
 
     /** @type CI_Controller */
